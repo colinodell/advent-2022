4d0a3817270ac4c7be59131a028cf3bf56329a88
Colin O'Dell <colinodell@gmail.com>
2022-11-30 17:43:06 -0500

    Run checks before each commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "advent-2022"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.24"
+}
+
+gitHooks {
+    preCommit {
+        from {
+            """
+            ./gradlew check
+            """.trimIndent()
+        }
+    }
+    createHooks(true)
+}
