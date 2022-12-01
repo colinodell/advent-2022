067c7f75060641333dfedbb60b077e8572595552
Colin O'Dell <colinodell@gmail.com>
2022-12-01 07:20:32 -0500

    Day 1: code reuse

--- src/main/kotlin/com/colinodell/advent2022/Day01.kt
+++ src/main/kotlin/com/colinodell/advent2022/Day01.kt
@@ -1,17 +1,16 @@
 package com.colinodell.advent2022
 
-class Day01(private val input: List<String>) {
+class Day01(input: List<String>) {
+    private val elfCalories = input
+        .chunkedBy { it.isEmpty() }
+        .map { it.sumOf { it.toInt() } }
+
     fun solvePart1(): Int {
-        return input
-            .chunkedBy { it.isEmpty() }
-            .map { it.sumOf { it.toInt() } }
-            .max()
+        return elfCalories.max()
     }
 
     fun solvePart2(): Int {
-        return input
-            .chunkedBy { it.isEmpty() }
-            .map { it.sumOf { it.toInt() } }
+        return elfCalories
             .sortedDescending()
             .take(3)
             .sum()
