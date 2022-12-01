dd162ec8dc7fc3c45cf59f64df75b33232a0d5be
Colin O'Dell <colinodell@gmail.com>
2022-12-01 07:16:35 -0500

    Day 1: reimplement chunkedBy using fold

--- src/main/kotlin/com/colinodell/advent2022/Extensions.kt
+++ src/main/kotlin/com/colinodell/advent2022/Extensions.kt
@@ -1,20 +1,11 @@
 package com.colinodell.advent2022
 
-fun <T> Iterable<T>.chunkedBy(separator: (T) -> Boolean): Sequence<List<T>> {
-    return sequence {
-        val chunk = mutableListOf<T>()
-        for (item in this@chunkedBy) {
-            if (separator(item)) {
-                if (chunk.isNotEmpty()) {
-                    yield(chunk.toList())
-                    chunk.clear()
-                }
-            } else {
-                chunk.add(item)
-            }
+fun <T> Iterable<T>.chunkedBy(separator: (T) -> Boolean): List<List<T>> =
+    fold(mutableListOf(mutableListOf<T>())) { acc, t ->
+        if (separator(t)) {
+            acc.add(mutableListOf())
+        } else {
+            acc.last().add(t)
         }
-        if (chunk.isNotEmpty()) {
-            yield(chunk)
-        }
+        acc
     }
-}
