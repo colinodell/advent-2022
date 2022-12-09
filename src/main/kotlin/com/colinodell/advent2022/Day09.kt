3bb76727ba966002ece485ecf82e3416a468136f
Colin O'Dell <colinodell@gmail.com>
2022-12-09 11:33:39 -0500

    Refactor day 9 to avoid recursive sequences

--- src/main/kotlin/com/colinodell/advent2022/Day09.kt
+++ src/main/kotlin/com/colinodell/advent2022/Day09.kt
@@ -13,45 +13,32 @@ class Day09(input: List<String>) {
         Pair(directions[parts[0]]!!, parts[1].toInt())
     }
 
-    fun solvePart1() = follow(headKnot, 1).distinct().count()
-    fun solvePart2() = follow(headKnot, 9).distinct().count()
+    fun solvePart1() = solve(2)
+    fun solvePart2() = solve(10)
 
-    private val headKnot = sequence {
-        var pos = Vector2(0, 0)
-        yield(pos)
+    private fun solve(n: Int): Int {
+        // Create a list with all knots
+        val allKnots = List(n) { Vector2(0, 0) }.toMutableList()
+        // And a set to track where the tail has been
+        val tailLocations = mutableSetOf(allKnots.last())
 
-        for (step in steps) {
-            repeat(step.second) {
-                pos += step.first
-                yield(pos)
+        // Move the head knot according to our input
+        for ((dir, count) in steps) {
+            // Move in the given direction (`dir`) `count` times
+            repeat(count) {
+                // The first knot always moves in the direction given
+                allKnots[0] += dir
+                // All other knots only move towards the previous one if they aren't touching
+                for (i in 1 until n) {
+                    if (!allKnots[i].isTouching(allKnots[i - 1])) {
+                        allKnots[i] += (allKnots[i - 1] - allKnots[i]).normalize()
+                    }
+                }
+                // Store the position of the tail knot
+                tailLocations.add(allKnots.last())
             }
         }
-    }
 
-    /**
-     * Generate a rope with (followerCount+1) knots and follow the tail knot
-     */
-    private fun follow(headKnotPositions: Sequence<Vector2>, followerCount: Int) = sequence {
-        var seq = headKnotPositions
-        repeat(followerCount) {
-            seq = follow(seq)
-        }
-        yieldAll(seq)
-    }
-
-    /**
-     * Generate a sequence of positions to follow the given knot
-     */
-    private fun follow(positions: Sequence<Vector2>) = sequence {
-        var followerPos = Vector2(0, 0)
-        yield(followerPos)
-
-        for (leadPos in positions) {
-            while (!followerPos.isTouching(leadPos)) {
-                // Move towards the head position one step at a time
-                followerPos += (leadPos - followerPos).normalize()
-                yield(followerPos)
-            }
-        }
+        return tailLocations.size
     }
 }
