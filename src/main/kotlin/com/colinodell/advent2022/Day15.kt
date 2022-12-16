fc5a0955c14ce2c200d836c454a9cab8bfb2f73d
Colin O'Dell <colinodell@gmail.com>
2022-12-15 20:35:02 -0500

    Rename some variables

--- src/main/kotlin/com/colinodell/advent2022/Day15.kt
+++ src/main/kotlin/com/colinodell/advent2022/Day15.kt
@@ -6,8 +6,8 @@ class Day15(input: List<String>) {
     private val regex = Regex("x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)")
 
     private val sensors = input.map { line ->
-        regex.find(line)!!.destructured.let { (x, y, bx, by) ->
-            val sensor = Vector2(x.toInt(), y.toInt())
+        regex.find(line)!!.destructured.let { (sx, sy, bx, by) ->
+            val sensor = Vector2(sx.toInt(), sy.toInt())
             val beacon = Vector2(bx.toInt(), by.toInt())
             Sensor(sensor, beacon)
         }
@@ -15,9 +15,9 @@ class Day15(input: List<String>) {
 
     fun solvePart1(row: Int) = findDisallowedPositions(row).sumOf { it.count() } - 1
 
-    fun solvePart2(max: Int): Long {
-        for (row in 0..max) {
-            val disallowedRanges = findDisallowedPositions(row).clamp(0, 4000000)
+    fun solvePart2(maxY: Int): Long {
+        for (y in 0..maxY) {
+            val disallowedRanges = findDisallowedPositions(y).clamp(0, 4000000)
 
             // Assume the distress beacon is not coming from the very edges of our search area
             // With this assumption, we can simply check if the range has a gap somewhere in the middle
@@ -28,16 +28,16 @@ class Day15(input: List<String>) {
             // Gap found! Assume there's only one gap, so the x coordinate is just 1 higher than the first range
             val x = disallowedRanges[0].last + 1
 
-            return x.toLong() * 4000000L + row.toLong()
+            return x.toLong() * 4000000L + y.toLong()
         }
 
         error("No solution found")
     }
 
-    private fun findDisallowedPositions(row: Int): List<IntRange> {
+    private fun findDisallowedPositions(y: Int): List<IntRange> {
         val disallowedRanges = mutableSetOf<IntRange>()
         for (sensor in sensors) {
-            val dy = abs(row - sensor.position.y)
+            val dy = abs(y - sensor.position.y)
             if (dy > sensor.radius) {
                 continue
             }
