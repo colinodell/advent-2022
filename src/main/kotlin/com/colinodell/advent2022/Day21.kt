0f61c507e3a48403d56a86362d19208e62789f50
Colin O'Dell <colinodell@gmail.com>
2022-12-21 09:33:22 -0500

    Refactor into separate classes

--- src/main/kotlin/com/colinodell/advent2022/Day21.kt
+++ src/main/kotlin/com/colinodell/advent2022/Day21.kt
@@ -1,40 +1,50 @@
 package com.colinodell.advent2022
 
 class Day21(input: List<String>) {
-    private val monkeys = input.associate {
-        val (name, job) = it.split(": ")
-        name to Monkey(job)
+    private val monkeys = input
+        .map { line ->
+            val parts = line.split(":? ".toRegex())
+            if (parts.size == 2) {
+                NumberMonkey(parts[0], parts[1].toLong())
+            } else {
+                JobMonkey(parts[0], parts[1], parts[3], parts[2])
+            }
+        }
+        .associateBy { it.name }
+        .apply {
+            values.forEach { m ->
+                if (m is JobMonkey) {
+                    m.associate(this)
+                }
+            }
+        }
+
+    fun solvePart1() = monkeys["root"]!!.yell()
+
+
+    private interface Monkey {
+        fun yell(): Long
     }
 
-    fun solvePart1() = monkeys["root"]!!.yell(monkeys)
+    private class NumberMonkey(private val value: Long) : Monkey {
+        override fun yell() = value
+    }
 
-    private class Monkey(private var job: String) {
-        private var finalValue: Long? = null
+    private class JobMonkey(private val leftName: String, private val rightName: String, private val operator: String) : Monkey {
+        private lateinit var left: Monkey
+        private lateinit var right: Monkey
 
-        fun yell(monkeys: Map<String, Monkey>): Long {
-            if (finalValue != null) {
-                return finalValue!!
-            }
+        fun associate(monkeys: Map<String, Monkey>) {
+            left = monkeys[leftName]!!
+            right = monkeys[rightName]!!
+        }
 
-            val parts = job.split(" ")
-            if (parts.size == 1) {
-                finalValue = parts[0].toLong()
-                return finalValue!!
-            }
-
-            val op = parts[1]
-            val a = monkeys[parts[0]]!!.yell(monkeys)
-            val b = monkeys[parts[2]]!!.yell(monkeys)
-
-            finalValue = when (op) {
-                "+" -> a + b
-                "-" -> a - b
-                "*" -> a * b
-                "/" -> a / b
-                else -> throw IllegalArgumentException("Unknown op: $op")
-            }
-
-            return finalValue!!
+        override fun yell() = when (operator) {
+            "+" -> left.yell() + right.yell()
+            "-" -> left.yell() - right.yell()
+            "*" -> left.yell() * right.yell()
+            "/" -> left.yell() / right.yell()
+            else -> throw IllegalArgumentException("Unknown operator: $operator")
         }
     }
 }
